1767f1dfdcec030bacbb7dd1e1628a2e8e78860e
NoRest4AWhearry <awhearry@gmail.com>
2014-09-27 17:47:42 -0400

    create navigation and add bootstrap styling

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
 
   resources :events
 
-  devise_for :users
+  devise_for :users, :path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout'}
 
 	resources :users, only: [:index, :show] do
 		member do
