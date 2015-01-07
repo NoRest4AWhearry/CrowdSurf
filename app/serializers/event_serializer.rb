303a843a45f8baba88a9729cbc7517b13cb8a45a
Allen Whearry <awhearry@gmail.com>
2015-01-07 00:55:11 -0500

    minor change

--- app/serializers/event_serializer.rb
+++ app/serializers/event_serializer.rb
@@ -3,6 +3,4 @@ class EventSerializer < ActiveModel::Serializer
              :longitude, :latitude, :youtube_id, :flyer_uid, :flyer_name,
              :details, :start, :end, :created_at
 
-  has_one :city, :state, :category, :user
-  has_many :ticket_types, :attendees
 end
